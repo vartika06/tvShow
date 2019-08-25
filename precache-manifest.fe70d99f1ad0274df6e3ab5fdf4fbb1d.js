63bc88429b524b34ce92ed832d4d2b90f6de7b26
vartika06 <vartika.1998@gmail.com>
2019-08-25 17:06:05 +0530

    Updates

---
+++ precache-manifest.fe70d99f1ad0274df6e3ab5fdf4fbb1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57eaec8832ce723f1fa898c7ed00041d",
+    "url": "/tvShow/index.html"
+  },
+  {
+    "revision": "8b6f9ad3c27c6255f408",
+    "url": "/tvShow/static/css/main.6311bbb8.chunk.css"
+  },
+  {
+    "revision": "a7910de2a175f66c284e",
+    "url": "/tvShow/static/js/2.ef4e896f.chunk.js"
+  },
+  {
+    "revision": "8b6f9ad3c27c6255f408",
+    "url": "/tvShow/static/js/main.56769d43.chunk.js"
+  },
+  {
+    "revision": "accfc19e281d3688f38a",
+    "url": "/tvShow/static/js/runtime~main.013ce22f.js"
+  }
+]);
